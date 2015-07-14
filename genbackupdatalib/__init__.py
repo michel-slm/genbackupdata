f0bacefe4cf04e1cf74ff400bf68522ff1824b8f
Lars Wirzenius <liw@liw.fi>
2015-07-14 18:07:51 +0300

    Prepare release version 1.8

--- genbackupdatalib/__init__.py
+++ genbackupdatalib/__init__.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-__version__ = '1.7'
+__version__ = '1.8'
 
 from .generator import DataGenerator
 from .names import NameGenerator
